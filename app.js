aa27fc49815434fd3a078869a01db74859c31ec0
anishazahan <anishazahan13@gmail.com>
2022-03-02 22:28:13 +0600

    error handling

--- app.js
+++ app.js
@@ -4,24 +4,15 @@
 
 const searchButton= () =>{
 
-    // console.log(hellow);
     const inputField = document.getElementById('input-field');
     const inputText = inputField.value;
-    const inputError = document.getElementById('input-error');
-    // console.log(inputText);
-
-    // if(inputText === "number" || inputText =="" || inputText <=0){
-    //     inputError.innerText = "No Results Found";
-    //     inputField.value = '';
-    //     inputField.textContent ='';
-       
-    // }
-
+    document.getElementById('phone-details').textContent = "";
+    // const inputError = document.getElementById('input-error');
         const url = `https://openapi.programming-hero.com/api/phones?search=${inputText}`
         fetch (url)
         .then(res => res.json())
         .then(data =>loadingPhones(data.data));
-    //    console.log(data);
+   
         // inputError.innerText = "";
     
     
@@ -33,12 +24,18 @@ const searchButton= () =>{
 const loadingPhones = (phones) => {
     // console.log(phones);
     const phoneResults = document.getElementById('show-phones');
-    // phoneResults.textContent = '';
+    phoneResults.textContent = '';
 
-    for(const phone of phones){
+    if( phones.length === 0){
+      document.getElementById('input-error').innerText ="No result found"
+       
+    }else{
+      
+      for(const phone of phones){
         // console.log(phone.slug);
         const div = document.createElement('div');
         div.classList.add('col-lg-4');
+        div.classList.add('col-md-6');
         div.innerHTML = ` <div class="phone p-4 border mb-3 shadow">
                 <div class="img">
                 <img src = "${phone.image}"></div>
@@ -53,7 +50,11 @@ const loadingPhones = (phones) => {
                 </div>
                 </div>`
                 phoneResults.appendChild(div);
+                document.getElementById('input-error').innerText ="";
     }
+
+    }
+   
 
 
 }
@@ -72,50 +73,87 @@ const phoneDetails = (idDetails) =>{
   // .......show phone details.........
 
 
-  // const detailsContainer = document.getElementById('phone-details').style.display = 'none';
-  const showIdPhoneDetails = phone =>{
-    // console.log(phone);
-    const phoneDetails = document.getElementById('phone-details');
-    // phoneDetails.textContent ='';
-    const div = document.createElement('div');
   
-    div.innerHTML =` <div class="dismiss-btn-parent mt-0">
-                <button class="dismiss-btn" id="dismiss">x</button>
+  const showIdPhoneDetails = phone =>{
+   
+    const phoneDetails = document.getElementById('phone-details');
+    const others = phone.others;
+    
+    phoneDetails.textContent ='';
+    const div = document.createElement('div');
+    div.classList.add("col-md-6");
+    div.classList.add("mx-auto");
+  
+    div.innerHTML =`<div class="card card-details shadow border">
+            <div class="text-center">
+            <img src="${phone.image}" class="img-fluid rounded-start details-img" alt="...">
             </div>
-            <div class="row" >
-              <div class="col-md-4">
-                  
-                <img src="${phone.image}" class="img-fluid rounded-start details-img" alt="...">
-              </div>
-              <div class="col-md-8 ">
-                <div class="card-body">
-                  <h5 class="phone-name">${phone.name}</h5>
-                  <p  class="phone-brand">Brand:${phone.brand}</p>
-                  <h5 class="feature-tittle">MainFeatures</h5>
-                  <div class="top-features">
-                    <p class="feature">ReleaseDate:${phone.releaseDate}</p>
-                    <p class="feature">DisplaySize:</p>
-                    <p class="feature">ChipSet:</p>
-                    <p class="feature">memory:</p>
-                    <p class="feature">Sensors:</p>
-                    <p class="feature">()</p>
-                    
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="other-features">
-                <h5 class="feature-tittle">Other Features</h5>
-                <div class="top-features">
-                    <p class="feature">WLAN:</p>
-                    <p class="feature">Bluetooth:</p>
-                    <p class="feature">GPS:</p>
-                    <p class="feature">NFC:</p>
-                    <p class="feature">Radio:</p>
-                    <p class="feature">USB:</p>
-                    
-                  </div>
+            <h5 class="phone-name">${phone.name}</h5>
+             <p  class="phone-brand">Brand:${phone.brand}</p>
+            <h5 class="feature-tittle">MainFeatures</h5>
+            <div class="top-features">
+        <p class="feature">ReleaseDate: ${phone.releaseDate? phone.releaseDate:"Upcoming."}</p>
+          <p class="feature">DisplaySize: ${phone.mainFeatures.displaySize}</p>
+            <p class="feature">ChipSet: ${phone.mainFeatures.chipSet}</p>
+          <p class="feature">Memory: ${phone.mainFeatures.memory}</p>
+          <p class="feature">Sensors: ${phone.mainFeatures.sensors.toString()}</p>
+          <p class="feature-tittle">Other Features:${others == null? "No others data.":Object.entries(others)}</p>
+        
+        </div>
+              
 
-            </div>`
+        </div>`
      phoneDetails.appendChild(div);
+     
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   <div class="col-md-4">
+//   <img src="${phone.image}" class="img-fluid rounded-start details-img" alt="...">
+// </div>
+// <div class="col-md-8 ">
+//   <div class="card-body">
+//     <h5 class="phone-name">${phone.name}</h5>
+//     <p  class="phone-brand">Brand:${phone.brand}</p>
+//     <h5 class="feature-tittle">MainFeatures</h5>
+//     <div class="top-features">
+//       <p class="feature">ReleaseDate:${phone.releaseDate}</p>
+//       <p class="feature">DisplaySize:</p>
+//       <p class="feature">ChipSet:</p>
+//       <p class="feature">memory:</p>
+//       <p class="feature">Sensors:${phone.mainFeatures.sensors.toString()}</p>
+
+//     </div>
+//   </div>
+// </div>
+// </div>
+// <div class="other-features">
+//   <h5 class="feature-tittle">Other Features</h5>
+  // <div class="top-features">
+  //     <p class="feature">WLAN:</p>
+  //     <p class="feature">Bluetooth:</p>
+  //     <p class="feature">GPS:</p>
+  //     <p class="feature">NFC:</p>
+  //     <p class="feature">Radio:</p>
+  //     <p class="feature">USB:</p>
+  //   </div>
+// </div>
